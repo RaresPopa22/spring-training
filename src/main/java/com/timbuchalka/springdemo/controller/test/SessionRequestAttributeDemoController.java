4d7c44392217b09fa79e1e73647ca1e09fd5bc3c
Rares <poparares22@yahoo.com>
2019-01-11 20:22:22 +0200

    working on spring-mvc-form-tags-demo
    partial commit getting my groove on

---
+++ src/main/java/com/timbuchalka/springdemo/controller/test/SessionRequestAttributeDemoController.java
@@ -0,0 +1,105 @@
+package com.timbuchalka.springdemo.controller.test;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.bind.annotation.SessionAttributes;
+import org.springframework.web.bind.support.SessionStatus;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.timbuchalka.springdemo.domain.test.Visitor;
+import com.timbuchalka.springdemo.domain.test.VisitorCount;
+import com.timbuchalka.springdemo.domain.test.VisitorData;
+import com.timbuchalka.springdemo.domain.test.VisitorService;
+
+@Controller
+@SessionAttributes(names= {"visitordata", "visitorcount"})
+@RequestMapping("/visitorRegister")
+public class SessionRequestAttributeDemoController {
+
+	@Autowired
+	public VisitorService visitorService;
+	
+	private static Logger LOGGER = 
+			LoggerFactory.getLogger(SessionRequestAttributeDemoController.class);
+	
+	@RequestMapping("/home")
+	public ModelAndView home() {
+		return new ModelAndView(
+				"test/sessionRequestAttributeViews/sessionRequestAttributeHome", 
+				"visitorstats", 
+				new VisitorData());
+	}	
+	
+	@ModelAttribute("visitordata")
+	public VisitorData addVisitorData() {
+		List<Visitor> visitors = new ArrayList<>();
+		VisitorData vData = new VisitorData(null, null, visitors);
+		
+		return vData;
+	}
+	
+	@ModelAttribute("visitorcount")
+	public VisitorCount countVisitors() {
+		return new VisitorCount(0);
+	}
+	
+	@RequestMapping(value = "/visitor", method = RequestMethod.POST)
+	public String getVisitors(@ModelAttribute("visitor") VisitorData curentVisitor,
+								HttpSession session,
+								SessionStatus sessionStatus,
+								HttpServletRequest request,
+								@SessionAttribute(name = "sessionStartTime") LocalDateTime startTime,
+								@RequestAttribute(name = "currentTime") LocalDateTime clockTime,
+								Model model) {
+		VisitorData visitorDataFromSession = (VisitorData) session.getAttribute("visitordata");
+		visitorService.registerVisitor(visitorDataFromSession, curentVisitor);
+		VisitorCount visitorCount = (VisitorCount) session.getAttribute("visitorcount");
+		visitorService.updateCount(visitorCount);
+		
+		Long currentSessionDuration = visitorService.computeDuration(startTime);
+		
+		if(visitorCount.getCount() == 5) {
+			sessionStatus.setComplete();
+//			session.invalidate();
+		}
+		
+//		model.addAttribute("timeHeading", visitorService.describeCurrentTime(clockTime));
+//		model.addAttribute("durationHeading", visitorService.describeCurrentDuration(currentSessionDuration));
+		Map<String, Object> modelMap = model.asMap();
+		modelMap.put("timeHeading", visitorService.describeCurrentTime(clockTime));
+		modelMap.put("durationHeading", visitorService.describeCurrentDuration(currentSessionDuration));
+		
+		return "test/sessionRequestAttributeViews/sessionRequestAttributeResult";
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/main/java/com/timbuchalka/springdemo/domain/test/OrganizationRegistration.java
@@ -0,0 +1,32 @@
+package com.timbuchalka.springdemo.domain.test;
+
+public class OrganizationRegistration {
+
+	private String orgName;
+	private String country;
+	private String turnover;
+
+	public String getOrgName() {
+		return orgName;
+	}
+
+	public void setOrgName(String orgName) {
+		this.orgName = orgName;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public String getTurnover() {
+		return turnover;
+	}
+
+	public void setTurnover(String turnover) {
+		this.turnover = turnover;
+	}
+}
